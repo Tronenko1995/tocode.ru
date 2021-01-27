3d85db78406a3267f591f1181ce90f02b464fbd3
alextr999 <tronenko.aleksandr@gmail.com>
2021-01-27 23:30:49 +0200

    vue 27.01.2021

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+
+    <!-- <header></header> -->
+
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+      <div id="app">
+        <h1>HELLO</h1>
+        <img alt="Vue logo" src="./assets/logo.png">
+      </div>
+        </div>
+      </section>
+    </div>
+
+    <!-- <footer></footer> -->
+    
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
