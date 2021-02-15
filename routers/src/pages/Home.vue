316353d788dce124a7a5d9f9f1d64e53c89e8622
alextr999 <tronenko.aleksandr@gmail.com>
2021-02-15 22:51:51 +0200

    home vue 15.02.2021

---
+++ routers/src/pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="wrapper">
+        <div class="wrapper-content wrapper-content--fixed">
+            <section>
+                <h1>Home page</h1>
+            </section>
+        </div>
+    </div>
+</template>
